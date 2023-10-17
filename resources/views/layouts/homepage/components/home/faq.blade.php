9867ee920d3dc65f3a80cc84b99815b81801f161
hainv198 <hainvth161197@gmail.com>
2023-10-17 17:56:28 +0700

    hainv add session faq 17102023

--- resources/views/layouts/homepage/components/home/faq.blade.php
+++ resources/views/layouts/homepage/components/home/faq.blade.php
@@ -13,25 +13,24 @@
                     <p class="text-description-banner">
                         {{__('UBRAND đi kèm với các tính năng và tích hợp cho phép bạn làm được nhiều việc hơn là chỉ chia sẻ thông tin liên hệ')}}
                     </p>
-
                 </div>
                 <div class="accordion" id="accordionExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                <b>{{__('Danh thiếp NFC tùy chỉnh')}}</b>
+                                <img src="{{ asset('landing/assets/img/icons/credit-card.svg') }}" alt="icon_footer" > <b class="accordion-title">{{__('Danh thiếp NFC tùy chỉnh')}}</b>
                             </button>
                         </h2>
                         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                {{__('Danh thiếp NFC thời trang và sáng tạo có thương hiệu, logo và thiết kế của bạn.')}}
+                                {{__('Danh thiếp NFC thời trang và sáng tạo có thương hiệu,')}}<br> {{__('logo và thiết kế của bạn.')}}
                             </div>
                         </div>
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingTwo">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Accordion Item #2
+                                <img src="{{ asset('landing/assets/img/icons/share.svg') }}" alt="icon_footer" > <b class="accordion-title">{{__('Trải nghiệm chia sẻ không giới hạn')}}</b>
                             </button>
                         </h2>
                         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -43,10 +42,46 @@
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingThree">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Accordion Item #3
+                                <img src="{{ asset('landing/assets/img/icons/share.svg') }}" alt="icon_footer" > <b class="accordion-title">{{__('Trải nghiệm chia sẻ không giới hạn')}}</b>
                             </button>
                         </h2>
                         <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                <img src="{{ asset('landing/assets/img/icons/profile.svg') }}" alt="icon_footer" > <b class="accordion-title">{{__('Trang hồ sơ độc đáo')}}</b>
+                            </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFive">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                <img src="{{ asset('landing/assets/img/icons/qr-code.svg') }}" alt="icon_footer" > <b class="accordion-title">{{__('Mã QR')}}</b>
+                            </button>
+                        </h2>
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSix">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                <img src="{{ asset('landing/assets/img/icons/customer.svg') }}" alt="icon_footer" > <b class="accordion-title">{{__('Hỗ trợ khách hàng')}}</b>
+                            </button>
+                        </h2>
+                        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
                                 <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                             </div>
--- storage/framework/views/86a76ac4631667a11faa7b7cc7ac06a370e21fc0.php
+++ storage/framework/views/86a76ac4631667a11faa7b7cc7ac06a370e21fc0.php
@@ -21,12 +21,12 @@
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                <b><?php echo e(__('Danh thiếp NFC tùy chỉnh')); ?></b>
+                                <img src="<?php echo e(asset('landing/assets/img/icons/credit-card.svg')); ?>" alt="icon_footer" > <b class="accordion-title"><?php echo e(__('Danh thiếp NFC tùy chỉnh')); ?></b>
                             </button>
                         </h2>
                         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                <?php echo e(__('Danh thiếp NFC thời trang và sáng tạo có thương hiệu, logo và thiết kế của bạn.')); ?>
+                                <?php echo e(__('Danh thiếp NFC thời trang và sáng tạo có thương hiệu,')); ?><br> <?php echo e(__('logo và thiết kế của bạn.')); ?>
 
                             </div>
                         </div>
@@ -34,7 +34,7 @@
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingTwo">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Accordion Item #2
+                                <img src="<?php echo e(asset('landing/assets/img/icons/share.svg')); ?>" alt="icon_footer" > <b class="accordion-title"><?php echo e(__('Trải nghiệm chia sẻ không giới hạn')); ?></b>
                             </button>
                         </h2>
                         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -46,10 +46,46 @@
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingThree">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Accordion Item #3
+                                <img src="<?php echo e(asset('landing/assets/img/icons/share.svg')); ?>" alt="icon_footer" > <b class="accordion-title"><?php echo e(__('Trải nghiệm chia sẻ không giới hạn')); ?></b>
                             </button>
                         </h2>
                         <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                <img src="<?php echo e(asset('landing/assets/img/icons/profile.svg')); ?>" alt="icon_footer" > <b class="accordion-title"><?php echo e(__('Trang hồ sơ độc đáo')); ?></b>
+                            </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFive">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                <img src="<?php echo e(asset('landing/assets/img/icons/qr-code.svg')); ?>" alt="icon_footer" > <b class="accordion-title"><?php echo e(__('Mã QR')); ?></b>
+                            </button>
+                        </h2>
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSix">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                <img src="<?php echo e(asset('landing/assets/img/icons/qr-code.svg')); ?>" alt="icon_footer" > <b class="accordion-title"><?php echo e(__('Hỗ trợ khách hàng')); ?></b>
+                            </button>
+                        </h2>
+                        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
                                 <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                             </div>
